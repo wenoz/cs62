901402c95e5da32e76f35cefacbeaf1c0f464e11
Wenonah Zhang <wzhang18@cmc.edu>
2017-08-31 15:48:42 -0700

    Update Circle.java
    added notes on override notion and equals function on circle class example

--- Lecture Examples/L2-Examples/Shapes/Circle.java
+++ Lecture Examples/L2-Examples/Shapes/Circle.java
@@ -8,22 +8,32 @@ class Circle extends Shape
 	
 	/** Construct a circle with a specified radius */
 	public Circle(double newRadius) {
-		radius = newRadius;
+		this.radius = newRadius; // equivalent to   radius = newRadius;
 	}
 	
 	/** Return radius */
 	public double getRadius() {
 		return radius;
 	}
-		
-	@Override
+	
+	public void setRadius(double  newRadius) {
+		this.radius = newRadius;
+	}
+			
+	@Override // highlight mistake if you mispell the function name from parent class
 	public double findArea() {
 		return radius * radius * Math.PI;
 	}
 	
 	@Override
+	public boolean equals(Object obj){
+		// because we passed an obj into the function, we have to cast it into the class 
+		return this.radius == ((Circle) obj)).radius );
+	}
+	
+	@Override
 	public String toString() {
-		return "Circle radius = " + getRadius();
+		return "Circle radius = " + this.radius; // + getRadius() does not work 
 	}
 }
 //////////////////////////////////////////////////////////////////
